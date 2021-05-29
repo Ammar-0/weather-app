2ab9b7b324cbf15934d300a5cdf611fb0f5411ac
Ammar-0 <83699054+Ammar-0@users.noreply.github.com>
2021-05-29 17:17:13 +0400

    A ReactJS web app to search the weather

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# weather-app
- A ReactJS web application that uses an external API to display the weather in a particular city 
+# Weather App
+
+A ReactJS web application that uses an external API to display the weather in a particular city. The application has a
+search feature to search for cities and displays the city name, date, temperature and the state of the weather in that city.
+
+# Project
+
+The project is complete and has been deployed using firebase.
+The link to the project is: https://my-weather-app-2f46c.web.app
+
+# Citations
+
+1. https://www.youtube.com/watch?v=GuA0_Z1llYU
---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+const api = {
+  key: "b7383266e4824e6298eb0c29a00480b4",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const searchLocaiton = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((response) => response.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = (inDate) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[inDate.getDay()];
+    let date = inDate.getDate();
+    let month = months[inDate.getMonth()];
+    let year = inDate.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  return (
+    <div
+      className={
+        typeof weather.main !== "undefined"
+          ? weather.main.temp < 20
+            ? "app cool"
+            : "app warm"
+          : "app"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search"
+            onChange={(event) => setQuery(event.target.value)}
+            value={query}
+            onKeyPress={searchLocaiton}
+          />
+        </div>
+        {typeof weather.name !== "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          <div className="welcome-screen">
+            Welcome to my weather app! Enter the name of a city to find out the
+            weather there.
+          </div>
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
